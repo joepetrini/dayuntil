9c29c72911291dfed92fc056e05faee35f153cf5
Joe Petrini <joepetrini@gmail.com>
2014-12-08 13:22:11 -0500

    Mobile changes

--- app/helpers.py
+++ app/helpers.py
@@ -4,8 +4,8 @@ from flask import request
 
 
 def _t(template_name):
-    if request.MOBILE:
-        return "mob_%s" % template_name
+    #if request.MOBILE:
+    return "mob_%s" % template_name
     return template_name
 
 
